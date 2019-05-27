6fc680181ed45d9e3cba81ee7661a94f85ee2f1e
erezuni <erez.hecht@mail.huji.ac.il>
2019-05-28 00:46:13 +0300

    added getAddressAt- which uses bit operators to get a specified spot of a given physical address (int)

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,32 +1,62 @@
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
+#include "string.h"
 
-void clearTable(uint64_t frameIndex) {
-    for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
+void clearTable(uint64_t frameIndex)
+{
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i)
+    {
         PMwrite(frameIndex * PAGE_SIZE + i, 0);
     }
 }
 
+/*
+ * Returns a part of the address at "spot" shift (from the left).
+ * */
+int getAddressAt(int paddress, int shift)
+{
+    int bitAnd = (1 << OFFSET_WIDTH) - 1; //Creates 1^OFFSET_WIDTH
+//    int addresses[TABLES_DEPTH + 1];
+    int i = 0;
+    while (i < shift)
+    {
+//        addresses[i]= paddress & bitAnd;
+        //Shift right by offset width to get the next spot:
+        paddress = (paddress >> OFFSET_WIDTH);
+        i++;
+    }
+
+    return (paddress & bitAnd);
+}
 
 
+void translateVaddress(uint64_t addr)
+{
+    int p = 0;
+    PMread(0 + addr, &p);
+}
 
-void VMinitialize() {
+
+void VMinitialize()
+{
     clearTable(0);
 }
 
 
-int VMread(uint64_t virtualAddress, word_t* value) {
+int VMread(uint64_t virtualAddress, word_t *value)
+{
     return 1;
 }
 
 
-int VMwrite(uint64_t virtualAddress, word_t value) {
+int VMwrite(uint64_t virtualAddress, word_t value)
+{
     return 1;
 }
 
 
 /*TODO Tasks:
-    1. Split the input address correctly to seperate parts: p1...pn,d.
+    1. Split the input address correctly to separate parts: p1...pn,d.
     2. Write a DFS algorithm that searches for an empty frame
     3. Handle Evicting pages
     4. Recursive function for handling each p_i, where each such recursion takes heed not to
